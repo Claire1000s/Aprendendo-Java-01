478d5a1e2ab9d63d1a54007aa8ae75061b8ed3bc
Leticia Ferreira <claire_thousands@hotmail.com>
2016-07-21 21:58:27 -0300

    ExerciciosSaidaSimples

---
+++ ExerciciosSaidaSimples.java
@@ -0,0 +1,108 @@
+package exerciciossaidasimples;
+
+public class ExerciciosSaidaSimples
+{
+    public static void main(String[] args)
+    {
+        /* 1. Frase na tela - Implemente um programa que escreve na tela a frase "O primeiro programa a
+gente nunca esquece!".*/
+        System.out.println("O primeiro programa a gente nunca esquece!");
+        
+        /* 2. Etiqueta - Elabore um programa que escreve seu nome completo na primeira linha, seu endereço
+        na segunda, e o CEP e telefone na terceira. */
+        System.out.print("Leticia da Silva Ramos Ferreira\nRua Antonio Fugolin, 252, Praia Azul, Americana - SP\n");
+        System.out.print("(19) 3467-1056 / (19) 9 9411-2623 / CEP: 13476-660\n");
+        
+        /* 3. Frases assassinas - Faça um programa que mostre na tela algumas frases assassinas, que são aquelas
+        que fazem com que muitas idéias sejam perdidas antes que amadureçam ou seja aprofundadas.
+        Eis alguns exemplos (bole também os seus):
+        "Isto não vai dar certo"
+        "Você nunca vai conseguir"
+        "Você vai se estrepar"
+        "Não vai dar em nada"
+        "Está tudo errado!" */
+        System.out.printf("Isto não vai dar certo!\nVoce nunca vai conseguir!\nVoce vai se estrepar!\n");
+        
+        /* 4. Mensagem - Escreva uma mensagem para uma pessoa de que goste. Implemente um programa
+que imprima essa mensagem. */
+        System.out.println("Lia, eu te amo!");
+        
+        /* 5. Ao mestre - Escreva um bilhete ao seu professor, informando seus objetivos nesta disciplina e o
+que espera dela e do professor. Implemente um programa que mostra seu bilhete na tela. */
+        System.out.println("Querido Perrotti,");
+        System.out.println("Não espero nada da sua disciplina, pq vc eh um bosta!");
+        System.out.println("Com amor,\nLeticia");
+        
+        /* 6. Quadrado - Escrever um programa que mostre a seguinte figura no alto da tela:
+        
+        XXXXX
+        X   X
+        X   X
+        X   X
+        XXXXX
+        */
+        System.out.print("\nXXXXX\nX   X\nX   X\nX   X\nXXXXX\n\n");
+        
+        /* 7. Tabela de notas - Escreva um programa que produza a seguinte saída na tela:
+        ALUNO(A)          NOTA
+        =========         =====
+        ALINE              9.0  
+        MÁRIO              DEZ
+        SÉRGIO             4.5    
+        SHIRLEY            7.0 */
+        
+        System.out.println("ALUNO (A)       NOTA");
+        System.out.println("=========       =====");
+        System.out.println("ALINE           9.0");
+        System.out.println("MÁRIO           DEZ");
+        System.out.println("SÉRGIO          4.5");
+        System.out.println("SHIRLEY         7.0\n\n");
+        
+        /* 8. Letra grande - Elabore um programa para produzir na tela a letra J, de Java Progressivo, usando
+        a própria. Se fosse ‘L’, seria assim:
+        L
+        L
+        L
+        LLLLL */
+        System.out.println(" JJJJJ");
+        System.out.println("   J  ");
+        System.out.println("   J  ");
+        System.out.println("J  J  ");
+        System.out.println(" J   \n");
+        
+        /* 9. Menu - Elabore um programa que mostre o seguinte menu na tela:
+        Cadastro de Clientes
+        0 - Fim
+        1 - Inclui
+        2 - Altera
+        3 - Exclui
+        4 - Consulta
+        Opção: */
+        
+        System.out.println("Cadastro de Clientes");
+        System.out.print("0 - Fim\n1- Inclui\n2- Altera\n3- Exclui\n4- Consulta\n\nOpção:\n\n");
+        
+        /* 10. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+Enriqueça o desenho com outros caracteres, simulando enfeites.
+                    X
+                   XXX
+                  XXXXX
+                 XXXXXXX
+                XXXXXXXXX
+               XXXXXXXXXXX
+              XXXXXXXXXXXXX
+             XXXXXXXXXXXXXXX
+                    XX
+                    XX
+                   XXXX */
+        System.out.println("    O     ");
+        System.out.println("   XXX    ");
+        System.out.println("  OXXXO   ");
+        System.out.println(" XXXOXXX  ");
+        System.out.println("OXXXXXXXO ");
+        System.out.println("   XXX    ");
+        System.out.println("   XXX    ");
+        System.out.println("   XXX    ");
+        System.out.println("  XXXXX   ");
+    } 
+}
